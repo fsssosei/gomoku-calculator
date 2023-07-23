22648fe946db8ad3029ffe71457127972dda73f2
qxtang <qx.tang@foxmail.com>
2023-07-23 13:46:52 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  configureWebpack: (config) => {
+    require('vux-loader').merge(config, {
+      options: {},
+      plugins: [
+        'vux-ui',
+        'duplicate-style',
+        {
+          name: 'less-theme',
+          path: 'src/theme.less',
+        },
+      ],
+    })
+  },
+
+  chainWebpack: (config) => {
+    // set worker-loader
+    config.module
+      .rule('worker')
+      .test(/\.worker\.js$/)
+      .use('worker-loader')
+      .loader('worker-loader')
+      .end()
+
+    // 解决：worker 热更新问题
+    config.module.rule('js').exclude.add(/\.worker\.js$/)
+  },
+
+  devServer: {
+    https: false,
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Resource-Policy': 'same-site',
+    },
+  },
+
+  pluginOptions: {
+    i18n: {
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
+
+  publicPath: process.env.NODE_ENV === 'production' ? '/ku10/gomoku-calculator' : '/',
+}
